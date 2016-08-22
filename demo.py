bea32dda968a4fbbef77a3a6881325e78f015c29
Jake Kara <jake@jakekara.com>
2016-08-22 15:16:45 -0400

    Initial

---
+++ demo.py
@@ -0,0 +1,13 @@
+import env, json
+from fec import Fec
+
+conn = Fec(env.API_KEY)
+
+def run():
+    ret = conn.get("/candidates",
+                   pages=[1,2,3,4,5],
+                   params="&cycle=2012&cycle=2016&office=P")
+    
+    print json.dumps(ret[3])
+
+run()
